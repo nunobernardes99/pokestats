d05c0b0ecaf16eec99e83db118758985512e5193
Nuno Bernardes <bernardes@coletiv.com>
2021-03-13 17:05:16 +0000

    Tests to poke_api.ex file

---
+++ test/pokestats/poke_api_test.exs
@@ -0,0 +1,39 @@
+defmodule Pokestats.PokeApiTest do
+  use ExUnit.Case, async: false
+
+  doctest Pokestats.PokeApi
+
+  test "list_pokemons/2" do
+    assert {:ok, pokemons_list} = Pokestats.PokeApi.list_pokemons()
+
+    first_pokemon = List.first(pokemons_list)
+    assert "bulbasaur" == first_pokemon["name"]
+    assert "https://pokeapi.co/api/v2/pokemon/1/" == first_pokemon["url"]
+  end
+
+  test "get_pokemon/1 by name" do
+    assert {:ok, pokemon} = Pokestats.PokeApi.get_pokemon("bulbasaur")
+    assert "bulbasaur" == pokemon.name
+    assert 2 == length(pokemon.type)
+    assert 7 == pokemon.measurements.height
+    assert 69 == pokemon.measurements.weight
+  end
+
+  test "get_pokemon/1 by id" do
+    assert {:ok, pokemon} = Pokestats.PokeApi.get_pokemon(1)
+    assert "bulbasaur" == pokemon.name
+    assert 2 == length(pokemon.type)
+    assert 7 == pokemon.measurements.height
+    assert 69 == pokemon.measurements.weight
+  end
+
+  test "get_pokemon/1 with invalid name" do
+    assert {:error, %{status: 404, message: "Not Found"}} ==
+             Pokestats.PokeApi.get_pokemon("bernardes")
+  end
+
+  test "get_pokemon/1 with invalid id" do
+    assert {:error, %{status: 404, message: "Not Found"}} ==
+             Pokestats.PokeApi.get_pokemon(0)
+  end
+end
