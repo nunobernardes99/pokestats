e0cc44e6dd554f6d516d71c5c2a40340b61a4e87
Nuno Bernardes <bernardes@coletiv.com>
2021-03-13 17:05:03 +0000

    Add code coverage dependency

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule Pokestats.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -54,7 +61,10 @@ defmodule Pokestats.MixProject do
       {:hackney, "~> 1.17.0"},
 
       # optional, required by JSON middleware
-      {:jason, "~> 1.0"}
+      {:jason, "~> 1.0"},
+
+      # Test coverage
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
