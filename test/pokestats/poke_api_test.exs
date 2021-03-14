fc6c37c6505c0eb4a4282e9040ab793c41af0e25
Nuno Bernardes <bernardes@coletiv.com>
2021-03-14 16:31:12 +0000

    Correct pokeapi tests

--- test/pokestats/poke_api_test.exs
+++ test/pokestats/poke_api_test.exs
@@ -9,24 +9,24 @@ defmodule Pokestats.PokeApiTest do
     first_pokemon = List.first(pokemons_list)
     assert "bulbasaur" == first_pokemon.name
     assert 2 == length(first_pokemon.type)
-    assert 7 == first_pokemon.measurements.height
-    assert 69 == first_pokemon.measurements.weight
+    assert 7 == first_pokemon.height
+    assert 69 == first_pokemon.weight
   end
 
   test "get_pokemon/1 by name" do
     assert {:ok, pokemon} = Pokestats.PokeApi.get_pokemon("bulbasaur")
     assert "bulbasaur" == pokemon.name
     assert 2 == length(pokemon.type)
-    assert 7 == pokemon.measurements.height
-    assert 69 == pokemon.measurements.weight
+    assert 7 == pokemon.height
+    assert 69 == pokemon.weight
   end
 
   test "get_pokemon/1 by id" do
     assert {:ok, pokemon} = Pokestats.PokeApi.get_pokemon(1)
     assert "bulbasaur" == pokemon.name
     assert 2 == length(pokemon.type)
-    assert 7 == pokemon.measurements.height
-    assert 69 == pokemon.measurements.weight
+    assert 7 == pokemon.height
+    assert 69 == pokemon.weight
   end
 
   test "get_pokemon/1 with invalid name" do
