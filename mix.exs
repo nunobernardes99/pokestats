df5ee155b3bd500001a4662e72164fb53445f529
Nuno Bernardes <bernardes@coletiv.com>
2021-03-14 15:14:13 +0000

    Increase mix version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pokestats.MixProject do
   def project do
     [
       app: :pokestats,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.11.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
