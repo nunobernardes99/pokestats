2209dfc8ed6e4689d896193d1d14a0ec91830649
Nuno Bernardes <bernardes@coletiv.com>
2021-03-14 16:31:37 +0000

    Pokemon Table schema and CRUD operations.

---
+++ lib/pokestats/schemas/pokemon.ex
@@ -0,0 +1,61 @@
+defmodule Pokestats.Pokemon do
+  @moduledoc """
+  This is the module responsible for the Pokemon schema
+  and basic CRUD operations with it
+  """
+  @moduledoc since: "0.1.3"
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Pokestats.Pokemon
+  alias Pokestats.Repo
+
+  schema "pokemon" do
+    field :name, :string
+    field :image, :string
+    field :height, :integer
+    field :weight, :integer
+    field :pokeapi_id, :integer
+    field :type, {:array, :string}
+    field :abilities, {:array, :string}
+
+    timestamps()
+  end
+
+  @required_fields [:name, :image, :height, :weight, :pokeapi_id, :type, :abilities]
+
+  @doc """
+  Validate `params` after manipulating struct
+  onto the database.
+  """
+  @doc since: "0.1.3"
+  def changeset(pokemon, params \\ %{}) do
+    pokemon
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
+  end
+
+  @doc """
+  Creates a pokemon onto our database with
+  the given `params`.
+
+  Returns {:ok %Pokemon{}}
+  """
+  @doc since: "0.1.3"
+  def create_pokemon(params) do
+    %Pokemon{}
+    |> changeset(params)
+    |> Repo.insert()
+  end
+
+  @doc """
+  List all pokemons of our database.
+
+  Returns {:ok, [%Pokemon{}]}
+  """
+  @doc since: "0.1.3"
+  def list_pokemons() do
+    %Pokemon{}
+    |> Repo.all()
+  end
+end
